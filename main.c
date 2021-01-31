b8ef2a3304df431efb7171bdc51866ca5d410b7a
空山新愚 <56070680+KonseXyu@users.noreply.github.com>
2021-01-31 22:13:11 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+/*
+	作者：杨帆 & 黄亿轩 
+	更新日期：2021.1.31 
+*/
+int main()
+{
+	 void menu();
+	 Menu();
+	 return 0;
+ } 
+ 
+ /*
+ 
+	README：
+	
+	程序首次启动时默认密码为空需要设置，如出现有首次启动无法设置登录密码的情况，
+	请删除根目录下的Password_Stu文件以重置密码文件。
+	
+	密码的加密方式：数字加3,小写字母变大写，大写字母减3
+	
+	如需改变读取文件的改变名字则找Menu
+	 
+	输入文件名字 
+	Inputfilename = "Stu_Info.txt"; 
+	
+	姓名代码文件名字 
+	Sidfilename = "S_Info2.txt" ;
+	
+	学院代码文件名字 
+	Scidfilename = "C_Code.txt";
+	
+	科目代码名字 
+	KMidfilename = "KM_info.txt" ; 
+	
+	输出文件名字 
+	Outfilename = "Stu_Info_Output.txt" ;
+	
+	密码信息 
+	Passwordname = "Password_Stu" ;
+	
+*/ 
+
